2051729ff145002596ce1ab16494882db7462601
KanishAnand <therealkanish@gmail.com>
2020-03-20 00:01:50 +0530

    trials

--- client.py
+++ client.py
@@ -8,11 +8,11 @@ API_ENDPOINT = 'http://10.4.21.147'
 PORT = 3000
 MAX_DEG = 11
 POPULATION_SIZE = 10
-GENERATIONS = 5
-TRAIN_RATIO = 0.5
-VAL_RATIO = 0.5
-# TEAM_ID = "MsOYrg4QoHcnSUht1hvbjhYM5BgzBcQT5HO3WVReiC338ykhP1"
-TEAM_ID_D = "hTGuBTgPhst20ZD8eZcFbCa53pWpgghVDSaKNBzn3DE2RDQEuz"
+GENERATIONS = 30
+TRAIN_RATIO = 0.6
+VAL_RATIO = 0.4
+TEAM_ID = "MsOYrg4QoHcnSUht1hvbjhYM5BgzBcQT5HO3WVReiC338ykhP1"
+## TEAM_ID_D = "hTGuBTgPhst20ZD8eZcFbCa53pWpgghVDSaKNBzn3DE2RDQEuz"
 # functions that you can call
 
 
@@ -92,36 +92,37 @@ def mutation(children):
 
 def ga():
     # # build up initial population
-    # wghts = [0.0, 0.1240317450077846, -6.211941063144333, 0.04933903144709126, 0.03810848157715883, 8.132366097133624e-05, -
-    #          6.018769160916912e-05, -1.251585565299179e-07, 3.484096383229681e-08, 4.1614924993407104e-11, -6.732420176902565e-12]
+    wghts = [0.0, 0.1240317450077846, -6.211941063144333, 0.04933903144709126, 0.03810848157715883, 8.132366097133624e-05, -
+             6.018769160916912e-05, -1.251585565299179e-07, 3.484096383229681e-08, 4.1614924993407104e-11, -6.732420176902565e-12]
 
-    # population = []
-    # indx = []
+    population = []
+    indx = []
 
-    # # building initial population
-    # for i in range(POPULATION_SIZE):
-    #     population.append(wghts.copy())
-    #     indx.append(i)
+    # building initial population
+    for i in range(POPULATION_SIZE):
+        population.append(wghts.copy())
+        indx.append(i)
 
-    # # adding noise to make initial population
-    # for i in range(POPULATION_SIZE):
-    #     # lst = list(np.random.normal(0, 1, 11))
-    #     for j in range(len(population[i])):
-    #         population[i][j] = population[i][j] + \
-    #             np.random.uniform(-1*1e-13, 1*1e-13)
-    #         population[i][j] = min(10, population[i][j])
-    #         population[i][j] = max(-10, population[i][j])
+    # adding noise to make initial population
+    for i in range(POPULATION_SIZE):
+        # lst = list(np.random.normal(0, 1, 11))
+        for j in range(len(population[i])):
+            population[i][j] = population[i][j] + \
+                np.random.uniform(-1*1e-13, 1*1e-13)
+            population[i][j] = min(10, population[i][j])
+            population[i][j] = max(-10, population[i][j])
 
-    # store_population = []
+    store_population = []
 
-    # for i in range(POPULATION_SIZE):
-    #     er = fit(population[i])
-    #     val = TRAIN_RATIO*er[0] + VAL_RATIO*er[1]
-    #     store_population.append((val, population[i], er[0], er[1]))
+    for i in range(POPULATION_SIZE):
+        er = fit(population[i])
+        # val = TRAIN_RATIO*er[0] + VAL_RATIO*er[1]
+        val = er[0]*er[1]*er[1]
+        store_population.append((val, population[i], er[0], er[1]))
 
     # use previous best output as initial population
-    with open('population.json') as f:
-        store_population = json.loads(f.read())
+    # with open('multiply_population.json') as f:
+    #     store_population = json.loads(f.read())
 
     gen = 0
     while gen < GENERATIONS:
@@ -175,7 +176,8 @@ def ga():
 
             # store this children
             er = fit(children)
-            val = TRAIN_RATIO*er[0] + VAL_RATIO*er[1]
+            # val = TRAIN_RATIO*er[0] + VAL_RATIO*er[1]
+            val = er[0]*er[1]*er[1]
             store_population.append((val, children, er[0], er[1]))
 
     if len(sys.argv) == 2 and sys.argv[1] == "SERVER":
@@ -195,7 +197,7 @@ def ga():
         #     if sz == POPULATION_SIZE:
         #         break
 
-        with open('population.json', 'w') as f:
+        with open('multiply_population.json', 'w') as f:
             f.write(json.dumps(store_population[:POPULATION_SIZE], indent=4))
 
 
@@ -208,8 +210,8 @@ if __name__ == "__main__":
     ga()
 
     # wghts = [0.0, 0.1240317450077846, -6.211941063144333, 0.04933903144709126, 0.03810848157715883, 8.132366097133624e-05, -
-    #  6.018769160916912e-05, -1.251585565299179e-07, 3.484096383229681e-08, 4.1614924993407104e-11, -6.732420176902565e-12]
-    # err = get_errors(TEAM_ID_D, wghts)
+    #          6.018769160916912e-05, -1.251585565299179e-07, 3.484096383229681e-08, 4.1614924993407104e-11, -6.732420176902565e-12]
+    # err = get_errors(TEAM_ID, wghts)
     # print(err)
     # assert len(err) == 2
 
