9ba790b0dc6f2f21db35cd2fd4a319d138e58428
KanishAnand <therealkanish@gmail.com>
2020-03-22 00:56:05 +0530

    tried with multiply

--- client_multiply.py
+++ client_multiply.py
@@ -9,8 +9,6 @@ PORT = 3000
 MAX_DEG = 11
 POPULATION_SIZE = 10
 GENERATIONS = 10
-TRAIN_RATIO = 0.3
-VAL_RATIO = 0.7
 TEAM_ID = "MsOYrg4QoHcnSUht1hvbjhYM5BgzBcQT5HO3WVReiC338ykhP1"
 # TEAM_ID_D = "hTGuBTgPhst20ZD8eZcFbCa53pWpgghVDSaKNBzn3DE2RDQEuz"
 # TEAM_ID_R = "QQ8vH8Upix6ai9hpg4nPdDnEyvDFSzVXJ87NWHk7gRQjEZnlym"
@@ -70,7 +68,7 @@ def fit(vector):
 
 
 def get_error(er):
-    return TRAIN_RATIO*er[0] + VAL_RATIO*er[1]
+    return er[0]*er[1]
 
 
 def crossover(ind, population):
@@ -84,26 +82,17 @@ def crossover(ind, population):
 def mutation(children):
     # adding some random number to any 4 elements of children
     ind = np.random.choice(children.shape[0], 5, replace=False)
-    # children[9] = children[9] + np.random.uniform(-1*1e-11, 1*1e-11)
-    # if(np.random.randint(-1, 1) > 0):
-    #     children[9] += 1e-12
-    # else:
-    #     children[9] += -1e-12
-    # children[9] = 4.006171586044872e-11
-    # children[9] = min(10, children[9])
-    # children[9] = max(-10, children[9])
-    # ind = [9, 10]
     for i in ind:
-        # children[i] = children[i] + np.random.uniform(-1*1e-13, 1*1e-13)
-        val = 0.0001
+        val = 1e-4
         if np.random.randint(-1, 1) == 0:
             val = -val
         else:
             pass
         children[i] += children[i]*val
+
+        # children[i] = children[i] + np.random.uniform(-1*1e-10, 1*1e-10)
         children[i] = min(10, children[i])
         children[i] = max(-10, children[i])
-
     return list(children)
 
 
@@ -111,6 +100,8 @@ def ga():
     # # build up initial population
     # wghts = [0.0, 0.1240317450077846, -6.211941063144333, 0.04933903144709126, 0.03810848157715883, 8.132366097133624e-05, -
     #          6.018769160916912e-05, -1.251585565299179e-07, 3.484096383229681e-08, 4.1614924993407104e-11, -6.732420176902565e-12]
+
+    # wghts = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
     # population = []
     # indx = []
@@ -133,13 +124,12 @@ def ga():
 
     # for i in range(POPULATION_SIZE):
     #     er = fit(population[i])
-    #   val = get_error(er)
+    # val = get_error(er)
     #     store_population.append((val, population[i], er[0], er[1]))
 
     # use previous best output as initial population
-    with open('population.json') as f:
+    with open('multiply_population.json') as f:
         store_population = json.loads(f.read())
-
     store_population1 = []
 
     for pop in store_population:
@@ -162,6 +152,8 @@ def ga():
         for pop in store_population:
             key = pop[0]
             val = pop[1]
+            # print(key, val)
+            # print('\n')
             sz += 1
             population.append(val)
             error.append(key)
@@ -175,7 +167,6 @@ def ga():
 
         # convert errors value to probability
         for er in error:
-            # prob = np.exp(-er)/total
             prob = 1/(er*total)
             probability.append(prob)
 
@@ -208,7 +199,7 @@ def ga():
         print(store_population[0][2], store_population[0][3])
         print('\n')
 
-        with open('population.json', 'w') as f:
+        with open('multiply_population.json', 'w') as f:
             f.write(json.dumps(store_population[:POPULATION_SIZE], indent=4))
 
 
@@ -219,49 +210,9 @@ if __name__ == "__main__":
     """
 
     ga()
-
-    # wghts1 = [
-    #     -3.2213119791400407e-13,
-    #     0.12403174500837291,
-    #     -6.2119410631458996,
-    #     0.04933903144677353,
-    #     0.03810848157722921,
-    #     8.132366154783281e-5,
-    #     -6.0187691822916674e-5,
-    #     -1.2516173291412288e-7,
-    #     3.484098931034179e-8,
-    #     4.038278175216079e-11,
-    #     -6.6990120153011976e-12
-    # ]
-    # org_wghts = [
-    #     0.0,
-    #     0.1240317450077846,
-    #     -6.211941063144333,
-    #     0.04933903144709126,
-    #     0.03810848157715883,
-    #     8.132366097133624e-05,
-    #     -6.018769160916912e-05,
-    #     -1.251585565299179e-07,
-    #     3.484096383229681e-08,
-    #     4.1614924993407104e-11,
-    #     -6.732420176902565e-12
-    # ]
-
-    # wghts = [
-    #     -9.999899999000009,
-    #     0.23762054053960596,
-    #     -6.189314049841086,
-    #     0.05308613024608479,
-    #     0.038114578802082695,
-    #     8.164617935743933e-05,
-    #     -6.0161214202060964e-05,
-    #     -1.2369648679130476e-07,
-    #     3.4837675203748266e-08,
-    #     3.922415070244515e-11,
-    #     -6.701960093776761e-12
-    # ]
-
-    # err = get_errors(TEAM_ID, wghts)
+    # wghts = [0.0, 0.1240317450077846, -6.211941063144333, 0.04933903144709126, 0.03810848157715883, 8.132366097133624e-05, -
+    #          6.018769160916912e-05, -1.251585565299179e-07, 3.484096383229681e-08, 4.1614924993407104e-11, -6.732420176902565e-12]
+    # err = get_errors(TEAM_ID_D, wghts)
     # print(err)
     # assert len(err) == 2
 
